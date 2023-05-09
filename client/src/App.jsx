c79e75744868126f5c183579057bbf0d6749085c
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-05-09 23:04:24 +0530

    routes/user/login added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,101 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { useState } from "react";
 import "./App.css";
+import UserPage from "./pages/userpage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [inputClass, SetinputClass] = useState("input");
-  const [overlayClass, SetoverlayClass] = useState("");
-  const [input, setInput] = useState("");
-  const [items, setItems] = useState([
-    "https://medium.com/latestTechs",
-    "mango",
-    "http://Github.com/Rudresh-pandey/linkVault",
-    "mango",
-    "apple",
-    "mango",
-    "apple",
-    "apple",
-    "mango",
-    "apple",
-    "mango",
-    "apple",
-  ]);
-  function addItem() {
-    setItems([...items, input]);
-  }
-  function inputdata(e) {
-    setInput(e.target.value);
-  }
   return (
-    <div>
-      <div className="main">
-        {/* <div className="input">
-        <input type="text" value={input} onChange={inputdata} />
-        <button onClick={addItem}>aDD</button>
-      </div> */}
-        <div className="list">
-          {items.map((item, index) => {
-            return (
-              <div className="item">
-                <div className="ham"></div>
-                <div className="listItem">
-                  <div className="itemInfo">
-                    <p>here will be the title ...</p>
-                    <p>15/03/2023</p>
-                  </div>
-                  <div className="itemUrl">
-                    <p>{item}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="add">
-          <button
-            id="addList"
-            onClick={() => {
-              SetinputClass("input active");
-              SetoverlayClass("active");
-            }}
-          >
-            <img src="images/addition.png" alt="" />
-          </button>
-        </div>
-      </div>
-
-      <div className={inputClass}>
-        <div className="inputoptions">
-          <div className="inputleft">
-            <div className="selectionBtn">
-              <button>
-                <img src="images/foldericon.png" alt="" />
-              </button>
-              <button>
-                <img src="images/fileicon.png" alt="" />
-              </button>
-            </div>
-            <div>
-              <input type="text" value={input} onChange={inputdata} />
-            </div>
-          </div>
-          <div className="inputright">
-            <button onClick={addItem} id="addBtn">
-              <img src="images/star.png" alt="" />
-            </button>
-          </div>
-        </div>
-        <button
-          id="closeBtn"
-          onClick={() => {
-            SetinputClass("input");
-            SetoverlayClass("");
-          }}
-        >
-          &times;
-        </button>
-      </div>
-      <div id="overlay" className={overlayClass}></div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />;
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

c3b574ee2f0f11ef68043c9aac8b84755ab37863
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-05-09 23:48:28 +0530

    login page ui update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./login.css";
 
 export default function LoginPage() {
   return (
-    <div>
-      <p>login</p>
+    <div className="container">
+      <div className="header">
+        <div className="logo">LINKvault</div>
+        <div className="loginBtn">
+          <button>sing in</button>
+          <button>sing up</button>
+        </div>
+      </div>
+      <div className="content">Container</div>
     </div>
   );
 }

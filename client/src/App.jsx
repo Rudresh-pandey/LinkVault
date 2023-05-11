99f0f182496d55dd61e06275a05ab3cec55dfdca
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-05-11 19:11:05 +0530

    thodaLeftRight hogaya Tha

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import UserPage from "./pages/UserPage";
 
 import IntroPage from "./pages/IntroPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<IntroPage />}>
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
         <Route exact path="/user" element={<UserPage />} />
       </Routes>

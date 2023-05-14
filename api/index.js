41c3cc8643bac403eaae6cd4a0432d26b18e3a55
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-05-14 13:41:27 +0530

    user_register_route_tested

--- api/index.js
+++ api/index.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+// var jsonParser = bodyParser.json();
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false }); app.use(bodyParser.json());
+app.use(jsonParser);
+
+const USERS = [];
+let USER_ID_COUNTER = 1;
+const middleware = (req, res, next) => {
+    console.log("middleware testing");
+    next();
+}
+
 
 app.get("/", (req, res) => {
-    res.send("ye");
+    res.send("IntroPage will be here");
+})
+
+app.post("/login", (req, res) => {
+
+})
+
+app.post("/register", (req, res) => {
+    const UserName = req.body.name;
+    const UserEmail = req.body.email;
+    const UserPass = req.body.pass;
+
+    if (USERS.find(x => x.UserEmail === UserEmail)) {
+        return res.status(403).json({ msg: "email already exists" });
+    }
+
+    USERS.push({
+        UserName, UserEmail, UserPass, id: USER_ID_COUNTER++
+    })
+
+    return res.json({
+        msg: "success",
+    })
+
+})
+
+app.get("/user", middleware, (req, res) => {
+    res.send("UserPage will be shown to user after user in logged in");
 })
 
 app.listen(3000, () => {

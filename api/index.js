92d25abe0dd53337178584ffca965d5490ad7e4d
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-05-28 23:05:38 +0530

    setting up post route

--- api/index.js
+++ api/index.js
@@ -1,49 +1,29 @@
 const express = require("express");
+const cors = require('cors');
+// const bodyParser = require("body-parser");
 const app = express();
-const bodyParser = require("body-parser");
-// var jsonParser = bodyParser.json();
 
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false }); app.use(bodyParser.json());
-app.use(jsonParser);
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-const USERS = [];
-let USER_ID_COUNTER = 1;
-const middleware = (req, res, next) => {
-    console.log("middleware testing");
-    next();
-}
+// app.use(bodyParser.json());
+app.use(express.json());
 
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}))
 
-app.get("/", (req, res) => {
-    res.send("IntroPage will be here");
+app.get('/test', (req, res) => {
+    res.json("ok")
 })
 
-app.post("/login", (req, res) => {
-
-})
-
-app.post("/register", (req, res) => {
-    const UserName = req.body.name;
-    const UserEmail = req.body.email;
-    const UserPass = req.body.pass;
-
-    if (USERS.find(x => x.UserEmail === UserEmail)) {
-        return res.status(403).json({ msg: "email already exists" });
-    }
-
-    USERS.push({
-        UserName, UserEmail, UserPass, id: USER_ID_COUNTER++
-    })
-
-    return res.json({
-        msg: "success",
-    })
-
-})
-
-app.get("/user", middleware, (req, res) => {
-    res.send("UserPage will be shown to user after user in logged in");
+app.post('/register', (req, res) => {
+    // console.log(res);
+    const { userName, userEmail, userPass } = req.body;
+    res.json({
+        userName, userPass, userEmail
+    });
+    // console.log(req.body);
 })
 
 app.listen(3000, () => {
